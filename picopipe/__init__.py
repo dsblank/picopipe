2fae899a87868a4991ea73d613eccb02901e2c49
Doug Blank <doug.blank@gmail.com>
2023-09-10 14:07:20 -0700

    WIP: names, codes, and mermaid

--- picopipe/__init__.py
+++ picopipe/__init__.py
@@ -7,17 +7,22 @@ import random
 import inspect
 
 def getsource(code):
-    try:
-        return inspect.getsource(code)
-    except Exception:
-        return code.__name__
+    if code.__name__ == "<lambda>":
+        return "lambda ...: ..."
+    elif hasattr(code, "_source"):
+        return code._source
+    else:
+        try:
+            return inspect.getsource(code)
+        except Exception:
+            return code.__name__
 
 def apply(step, outputs):
     return (step(_input) for _input in outputs)
 
-def pipeline(*steps, n_jobs=None, return_as="generator"):
-    if n_jobs in [None, 0]:
-        def pipe(inputs):
+def pipeline(*steps, name=None):
+    def pipe(inputs, n_jobs=None, return_as="generator"):
+        if n_jobs in [None, 0]:
             outputs = inputs
             for step in steps:
                 if hasattr(step, "_pfilter"):
@@ -29,55 +34,81 @@ def pipeline(*steps, n_jobs=None, return_as="generator"):
                 outputs = list(outputs)
 
             return outputs
-    else:
-        def pipe(inputs):
-            jobs = (joblib.delayed(pipeline(*steps, return_as="list"))([_input]) for _input in inputs)
-            return (results[0] for results in joblib.Parallel(n_jobs=n_jobs, return_as="generator")(jobs) if len(results) > 0)
+        else:
+            the_pipeline = pipeline(*steps)
+            jobs = (joblib.delayed(the_pipeline)([_input], return_as="list") for _input in inputs)
+            results = joblib.Parallel(n_jobs=n_jobs, return_as="generator")(jobs)
+            return (result[0] for result in results if len(result) > 0)
 
     pipe.__pipeline__ = {
         "type": "pipeline",
-        "steps": [step.__pipeline__ if hasattr(step, "__pipeline__")
-                  else {
-                          "type": "function",
-                          "name": step.__name__,
-                          "code": getsource(step)
-                  } for step in steps],
-        "n_jobs": n_jobs
+        "name": name if name else "pipeline",
+        "steps": [
+            step.__pipeline__ if hasattr(step, "__pipeline__")
+            else {
+                    "type": "function",
+                    "name": step.__name__,
+                    "code": getsource(step)
+            } for step in steps],
     }
     return pipe
 
 ## Pipeline functions:
 
 def to_mermaid(pipeline):
-    graph = _to_mermaid_recursive(pipeline.__pipeline__)
+    graph = _to_mermaid_recursive(pipeline.__pipeline__, [0])
     return graph
 
-def _to_mermaid_recursive(pipeline):
-    if hasattr(pipeline, "__pipeline__"):
-        if pipeline.__pipeline__["type"] == "pipeline":
-            for step in pipeline.__pipeline__["steps"]:
-                sub_part = to_mermaid_recusive(step)
-        elif pipeline.__pipeline__["type"] == "connection":
-            for sub_pipeline in pipeline.__pipeline__["pipelines"]:
-                sub_part = to_mermaid_recusive(sub_pipeline)
-        elif pipeline.__pipeline__["type"] == "step":
-            sub_part = pipeline
-        else:
-            raise Exception(f"unknown type: {pipeline.__pipeline__['type']}")
-    return
+def _makename(n):
+    return f"node{n}"
 
-def connect(*pipelines):
-    def connect(inputs):
+def _to_mermaid_recursive(pipeline, index):
+    if pipeline["type"] == "pipeline":
+        steps = pipeline["steps"]
+        subgraph = "subgraph {\n"
+        names = []
+        for step in steps:
+            names.append(_makename(index[0]))
+            subgraph += f"    {names[-1]}[\"{step['name']}\"]\n"
+            index[0] += 1
+        subgraph += "}\n"
+        if len(steps) <= 1:
+            arrows = ""
+        else:
+            arrows = ""
+            for s in range(len(names) - 1):
+                arrows += f"{names[s]} --> {names[s+1]}\n"
+        subgraph += arrows
+        return subgraph
+    elif pipeline["type"] == "connection":
+        subgraphs = ""
+        for sub_pipeline in pipeline["pipelines"]:
+            subgraphs += _to_mermaid_recursive(sub_pipeline, index)
+        return subgraphs
+    else:
+        raise Exception(f"unknown type: {pipeline}")
+
+def connect(*pipelines, name=None):
+    def connect(inputs, n_jobs=None):
         for pipeline in pipelines:
-            inputs = (lambda pipeline=pipeline: pipeline(inputs))()
+            inputs = (lambda pipeline=pipeline: pipeline(inputs, n_jobs=n_jobs))()
         return inputs
 
     connect.__pipeline__ = {
         "type": "connection",
+        "name": name if name else "connection",
         "pipelines": [pipeline.__pipeline__ for pipeline in pipelines],
     }
 
     return connect
+
+## Utility functions:
+
+def step(function, name):
+    if function.__name__ == "<lambda>":
+        function._source = "lambda ...: ..."
+    function.__name__ = name
+    return function
 
 ## Step functions:
 
@@ -106,7 +137,7 @@ def batch(inputs, size):
     for value in inputs:
         _batch.append(value)
         if (len(_batch) % size) == 0:
-            yield _batch.copy()
+            yield tuple(_batch)
             _batch.clear()
     if _batch:
         yield _batch
@@ -114,7 +145,4 @@ def batch(inputs, size):
 def sample(inputs, percent):
     """
     """
-    return filter(
-        lambda value: random.random() < percent,
-        inputs, 
-    )
+    return filter(lambda value: random.random() < percent, inputs)
